2ee3e9138ad2c0c9a0732884397ee6a8b7145a44
Eran Stiller <git@stiller.blog>
2018-08-22 16:01:34 +0300

    Basic single turn conversation bot recommending random beers

---
+++ src/BeerBot/BeerBot.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using BeerBot.BeerApiClient;
+using Microsoft.Bot;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+namespace BeerBot
+{
+    public class BeerBot : IBot
+    {
+        private readonly IBeerApi _beerService;
+
+        public BeerBot(IBeerApi beerService)
+        {
+            _beerService = beerService;
+        }
+
+        public async Task OnTurn(ITurnContext context)
+        {
+            switch (context.Activity.Type)
+            {
+                case ActivityTypes.Message:
+                    await HandleMessage(context);
+                    if (!context.Responded)
+                    {
+                        await context.SendActivity("I didn't quite understand what you are saying! You can type \"help\" for more information");
+                    }
+                    break;
+                case ActivityTypes.ConversationUpdate:
+                    await GreetAddedMembers(context);
+                    break;
+            }
+
+        }
+
+        private async Task HandleMessage(ITurnContext context)
+        {
+            switch (context.Activity.Text)
+            {
+                case var text when Regex.IsMatch(text, "^(hi|hello|hola).*", RegexOptions.IgnoreCase):
+                    await context.SendActivity("Welcome to your friendly neighbourhood bot-tender! How can I help you?");
+                    break;
+                case var text when Regex.IsMatch(text, ".*random.*", RegexOptions.IgnoreCase):
+                    var beer = await _beerService.BeersRandomGetAsync();
+                    await context.SendActivity($"You should definitly get a {beer.Name}");
+                    break;
+                case var text when Regex.IsMatch(text, ".*help.*", RegexOptions.IgnoreCase):
+                    await context.SendActivity("You can type 'random' for getting a beer recommendation");
+                    break;
+                case var text when Regex.IsMatch(text, "^(bye|exit|adios).*", RegexOptions.IgnoreCase):
+                    await context.SendActivity("So soon? Oh well. See you later :)");
+                    break;
+            }
+        }
+
+        private Task GreetAddedMembers(ITurnContext context)
+        {
+            var newMember = context.Activity.MembersAdded.FirstOrDefault();
+            if (newMember != null && newMember.Id != context.Activity.Recipient.Id && !string.IsNullOrWhiteSpace(newMember.Name))
+            {
+                return context.SendActivity($"Howdy {newMember.Name}!");
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/BeerBot/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using BeerBot.BeerApiClient;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder.BotFramework;
+using Microsoft.Bot.Builder.Core.Extensions;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Builder.TraceExtensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BeerBot
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddBot<BeerBot>(options =>
+            {
+                options.CredentialProvider = new ConfigurationCredentialProvider(Configuration);
+
+                options.Middleware.Add(new CatchExceptionMiddleware<Exception>(async (context, exception) =>
+                {
+                    await context.TraceActivity("BeerBot Exception", exception);
+                    await context.SendActivity("Sorry, it looks like something went wrong!");
+                }));
+            });
+
+            services.AddSingleton<IBeerApi, BeerApi>(sp => new BeerApi(new Uri(Configuration.GetValue<string>("BeerApiBaseUrl"))));
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles()
+                .UseStaticFiles()
+                .UseBotFramework();
+        }
+    }
+}
